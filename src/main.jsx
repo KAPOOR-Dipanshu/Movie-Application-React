905f43c4048fc1f61fe8e63b6da46b83f17ffe07
Dipanshu Kapoor <dipanshu.kapoor2002@gmail.com>
2023-12-26 13:45:36 +0530

    started

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
-
+import { inject } from '@vercel/analytics';
+ 
 import {store} from "./store/store";
 import { Provider } from 'react-redux'
 
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
